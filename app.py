b9c47b4198a0936a7bbe78eded539cc1040c1e65
clone <umarbektokyo@icloud.com>
2023-05-15 20:05:20 +0900

    Created empty functions

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ app.config["SECRET_KEY"] = "2023_May_15_19_57"
 def home():
     return "Hello world"
 
+@app.route('/new')
+def new_comparison():
+    return "New Comparison"
+
+@app.route("/<int:comparison_id>")
+def comparison():
+    return "Comparison X"
+
+@app.route("/<int:comparison_id>/play")
+def play():
+    return "Play"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
